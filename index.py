d21c0abfbea3190bd2a9e3ad9a2e13a93ec99c85
Ronaldo Borges Guimarães <ronaldobgui@outlook.com>
2022-05-27 16:45:05 -0400

    salvando anexos em arquivo zip

--- index.py
+++ index.py
@@ -1,16 +1,42 @@
+from datetime import datetime
+import os
+from tempfile import NamedTemporaryFile, mkstemp
+from zipfile import ZipFile
 import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
-response = requests.get(url)
+print('Buscando dados da página web...')
+result = requests.get(url).text
 
-soup = BeautifulSoup(response.text, 'html.parser')
+print('Fazendo parse dos dados...')
+soup = BeautifulSoup(result, 'html.parser')
 
 numeros = ['I', 'II', 'III', 'IV']
 
-for tag in soup.find_all('a'):
-    text = str(tag.string)
-    if any(f'Anexo {numero}' in text for numero in numeros):
-        href = tag.get('href')
-        print(f'text: {text}')
-        print(f'href: {href}\n')
+def isValidAnexo(title):
+    return any(f'Anexo {numero}' in title for numero in numeros)
+
+def writeTempFile(bytes):
+    file = NamedTemporaryFile(delete=False)
+    file.write(bytes)
+    file.close()
+    return file.name
+
+date = datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
+
+print('Iniciando a criação do arquivo zip...')
+zip_name = f'Anexos_Rol_Procedimentos_Eventos_Saúde_{date}.zip'
+zip_path = f'{os.getcwd()}\{zip_name}'
+
+with ZipFile(zip_path, 'w') as zip:
+    for tag in soup.find_all('a'):
+        text = str(tag.string)
+        if isValidAnexo(text):
+            href = tag.get('href')
+            file_name = os.path.basename(href)
+            temp_name = writeTempFile(requests.get(href).content)
+            print(f'Adicionando o arquivo "{file_name}" ao zip...')
+            zip.write(temp_name)
+            os.unlink(temp_name)
+print(f'Arquivos salvo em: "{zip_path}"')
