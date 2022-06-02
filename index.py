9e77270705a9d1e594a2ec115c37bef90b06258e
Ronaldo Borges Guimarães <ronaldobgui@outlook.com>
2022-06-02 11:06:44 -0400

    criação da função baixarAnexos

--- index.py
+++ index.py
@@ -5,17 +5,9 @@ from zipfile import ZipFile
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
-print('Buscando dados da página web...')
-result = requests.get(url).text
-
-print('Fazendo parse dos dados...')
-soup = BeautifulSoup(result, 'html.parser')
-
-numeros = ['I', 'II', 'III', 'IV']
-
-def isValidAnexo(title):
+def isValidAnexo(title, numeros):
     return any(f'Anexo {numero}' in title for numero in numeros)
+
 
 def writeTempFile(bytes):
     file = NamedTemporaryFile(delete=False)
@@ -23,20 +15,34 @@ def writeTempFile(bytes):
     file.close()
     return file.name
 
-date = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
 
-print('Iniciando a criação do arquivo zip...')
-zip_name = f'Anexos_Rol_Procedimentos_Eventos_Saúde_{date}.zip'
-zip_path = os.path.join(os.getcwd(), zip_name)
+def baixarAnexos(url, numeros):
+    print('Buscando dados da página web...')
+    result = requests.get(url).text
 
-with ZipFile(zip_path, 'w') as zip:
-    for tag in soup.find_all('a'):
-        text = str(tag.string)
-        if isValidAnexo(text):
-            href = tag.get('href')
-            file_name = os.path.basename(href)
-            temp_name = writeTempFile(requests.get(href).content)
-            print(f'Adicionando o arquivo "{file_name}" ao zip...')
-            zip.write(temp_name, file_name)
-            os.unlink(temp_name)
-print(f'Arquivo salvo em: "{zip_path}"')
+    print('Fazendo parse dos dados...')
+    soup = BeautifulSoup(result, 'html.parser')
+
+    date = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+
+    print('Iniciando a criação do arquivo zip...')
+    zip_name = f'Anexos_{"_".join(numeros)}_Rol_Procedimentos_Eventos_Saúde_{date}.zip'
+    zip_path = os.path.join(os.getcwd(), zip_name)
+
+    with ZipFile(zip_path, 'w') as zip:
+        for tag in soup.find_all('a'):
+            text = str(tag.string)
+            if isValidAnexo(text, numeros):
+                href = tag.get('href')
+                file_name = os.path.basename(href)
+                temp_name = writeTempFile(requests.get(href).content)
+                print(f'Adicionando o arquivo "{file_name}" ao zip...')
+                zip.write(temp_name, file_name)
+                os.unlink(temp_name)
+    print(f'Arquivo salvo em: "{zip_path}"')
+
+
+baixarAnexos(
+    url='https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude',
+    numeros=['I', 'II', 'III', 'IV']
+)
